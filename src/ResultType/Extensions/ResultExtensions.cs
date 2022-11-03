0e82747701111fc2e92a5709c7de4ec5880f5e53
Shekar Reddy <getbetterid@gmail.com>
2022-11-04 03:25:18 +0530

    Update ResultExtensions.cs

--- src/ResultType/Extensions/ResultExtensions.cs
+++ src/ResultType/Extensions/ResultExtensions.cs
@@ -1,16 +1,8 @@
-﻿// Copyright (c) 2022 Tobias Streng, MIT license
-
+using System.Reflection;
 using System.Runtime.Serialization;
-
-namespace ResultType.Extensions;
 
 public static class ResultExtensions
 {
-    /// <summary>
-    /// Checks if the <see cref="Result"/> was not successful and throws the underlying exception, if available.
-    /// While re-throwing, it preserves the StackTrace. Returns the Result if successful, so that it can be quickly checked.
-    /// </summary>
-    /// <param name="result"></param>
     public static Result Check(this Result result)
     {
         if (!result.Success)
@@ -22,11 +14,6 @@ public static class ResultExtensions
         return result;
     }
 
-    /// <summary>
-    /// Checks if the <see cref="Result<typeparamref name="T"/>"/> was not successful and throws the underlying exception, if available.
-    /// While re-throwing, it preserves the StackTrace. Returns the Result if successful, so that it can be quickly checked.
-    /// </summary>
-    /// <param name="result"></param>
     public static Result<T> Check<T>(this Result<T> result) where T : class
     {
         if (!result.Success)
@@ -42,14 +29,29 @@ public static class ResultExtensions
     /// Preserves the StackTrace, so that the Exception can be catched and re-thrown over multiple layers.
     /// </summary>
     /// <remarks>https://stackoverflow.com/a/2085377</remarks>
-    private static void PreserveStackTrace(Exception exception)
-    {
-        var streamingContext = new StreamingContext(StreamingContextStates.CrossAppDomain);
-        var objectManager = new ObjectManager(null, streamingContext);
-        var serializationInfo = new SerializationInfo(exception.GetType(), new FormatterConverter());
+    //private static void PreserveStackTrace(Exception exception)
+    //{
+    //    var streamingContext = new StreamingContext(StreamingContextStates.CrossAppDomain);
+    //    var objectManager = new ObjectManager(null, streamingContext);
+    //    var serializationInfo = new SerializationInfo(exception.GetType(), new FormatterConverter());
 
-        exception.GetObjectData(serializationInfo, streamingContext);
-        objectManager.RegisterObject(exception, 1, serializationInfo);
-        objectManager.DoFixups();
+    //    exception.GetObjectData(serializationInfo, streamingContext);
+    //    objectManager.RegisterObject(exception, 1, serializationInfo);
+    //    objectManager.DoFixups();
+    //}
+
+    public static Exception PreserveStackTrace(this Exception ex)
+    {
+        System.Runtime.ExceptionServices.ExceptionDispatchInfo.Capture(ex).Throw();
+        return ex;
     }
+
+
+    //public static void PreserveStackTrace(this Exception ex)
+    //{
+    //    typeof(Exception).GetMethod("PrepForRemoting",
+    //        BindingFlags.NonPublic | BindingFlags.Instance)
+    //        ?.Invoke(ex, new object[0]);
+    //    throw ex;
+    //}
 }
